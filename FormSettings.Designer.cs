09908882a58d10b1430d2ba4857306562d14349e
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2022-10-09 15:15:20 +0200

    Update LibBingo

--- FormSettings.Designer.cs
+++ FormSettings.Designer.cs
@@ -36,15 +36,15 @@
             this.lblLogic = new System.Windows.Forms.Label();
             this.flpMenu = new System.Windows.Forms.FlowLayoutPanel();
             this.flpLogic = new System.Windows.Forms.FlowLayoutPanel();
+            this.chkRaceMode = new System.Windows.Forms.CheckBox();
             this.flpDungeons = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblDungeons = new System.Windows.Forms.Label();
             this.chkDRC = new System.Windows.Forms.CheckBox();
             this.chkFW = new System.Windows.Forms.CheckBox();
             this.chkTOTG = new System.Windows.Forms.CheckBox();
             this.chkFF = new System.Windows.Forms.CheckBox();
             this.chkET = new System.Windows.Forms.CheckBox();
             this.chkWT = new System.Windows.Forms.CheckBox();
-            this.chkRaceMode = new System.Windows.Forms.CheckBox();
-            this.lblDungeons = new System.Windows.Forms.Label();
             this.flpMenu.SuspendLayout();
             this.flpLogic.SuspendLayout();
             this.flpDungeons.SuspendLayout();
@@ -61,16 +61,17 @@
             this.flpMain.Location = new System.Drawing.Point(0, 0);
             this.flpMain.Margin = new System.Windows.Forms.Padding(0);
             this.flpMain.Name = "flpMain";
-            this.flpMain.Padding = new System.Windows.Forms.Padding(10);
-            this.flpMain.Size = new System.Drawing.Size(800, 450);
+            this.flpMain.Padding = new System.Windows.Forms.Padding(10, 12, 10, 12);
+            this.flpMain.Size = new System.Drawing.Size(800, 562);
             this.flpMain.TabIndex = 0;
             // 
             // btnGenerate
             // 
             this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGenerate.Location = new System.Drawing.Point(13, 49);
+            this.btnGenerate.Location = new System.Drawing.Point(13, 62);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(130, 56);
+            this.btnGenerate.Size = new System.Drawing.Size(130, 70);
             this.btnGenerate.TabIndex = 0;
             this.btnGenerate.Text = "Generate Grid";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -84,23 +85,24 @@
             // 
             this.lsbLogic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lsbLogic.FormattingEnabled = true;
-            this.lsbLogic.ItemHeight = 16;
+            this.lsbLogic.ItemHeight = 20;
             this.lsbLogic.Items.AddRange(new object[] {
             "Limited",
             "No Limit"});
-            this.lsbLogic.Location = new System.Drawing.Point(3, 34);
+            this.lsbLogic.Location = new System.Drawing.Point(3, 43);
+            this.lsbLogic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lsbLogic.Name = "lsbLogic";
-            this.lsbLogic.Size = new System.Drawing.Size(200, 34);
+            this.lsbLogic.Size = new System.Drawing.Size(200, 42);
             this.lsbLogic.Sorted = true;
             this.lsbLogic.TabIndex = 1;
             // 
             // lblLogic
             // 
             this.lblLogic.BackColor = System.Drawing.Color.Transparent;
-            this.lblLogic.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogic.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblLogic.Location = new System.Drawing.Point(3, 0);
             this.lblLogic.Name = "lblLogic";
-            this.lblLogic.Size = new System.Drawing.Size(200, 31);
+            this.lblLogic.Size = new System.Drawing.Size(200, 39);
             this.lblLogic.TabIndex = 2;
             this.lblLogic.Text = "Logic :";
             this.lblLogic.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -117,11 +119,11 @@
             this.flpMenu.Controls.Add(this.flpDungeons);
             this.flpMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.flpMenu.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
-            this.flpMenu.Location = new System.Drawing.Point(0, 322);
-            this.flpMenu.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.flpMenu.Location = new System.Drawing.Point(0, 402);
+            this.flpMenu.Margin = new System.Windows.Forms.Padding(0, 12, 0, 0);
             this.flpMenu.Name = "flpMenu";
-            this.flpMenu.Padding = new System.Windows.Forms.Padding(10);
-            this.flpMenu.Size = new System.Drawing.Size(800, 128);
+            this.flpMenu.Padding = new System.Windows.Forms.Padding(10, 12, 10, 12);
+            this.flpMenu.Size = new System.Drawing.Size(800, 160);
             this.flpMenu.TabIndex = 3;
             // 
             // flpLogic
@@ -130,10 +132,23 @@
             this.flpLogic.Controls.Add(this.lsbLogic);
             this.flpLogic.Controls.Add(this.chkRaceMode);
             this.flpLogic.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flpLogic.Location = new System.Drawing.Point(149, 3);
+            this.flpLogic.Location = new System.Drawing.Point(149, 4);
+            this.flpLogic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.flpLogic.Name = "flpLogic";
-            this.flpLogic.Size = new System.Drawing.Size(205, 102);
+            this.flpLogic.Size = new System.Drawing.Size(205, 128);
             this.flpLogic.TabIndex = 3;
+            // 
+            // chkRaceMode
+            // 
+            this.chkRaceMode.AutoSize = true;
+            this.chkRaceMode.Location = new System.Drawing.Point(3, 93);
+            this.chkRaceMode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chkRaceMode.Name = "chkRaceMode";
+            this.chkRaceMode.Size = new System.Drawing.Size(106, 24);
+            this.chkRaceMode.TabIndex = 3;
+            this.chkRaceMode.Text = "Race Mode";
+            this.chkRaceMode.UseVisualStyleBackColor = true;
+            this.chkRaceMode.CheckedChanged += new System.EventHandler(this.chkRaceMode_CheckedChanged);
             // 
             // flpDungeons
             // 
@@ -145,18 +160,30 @@
             this.flpDungeons.Controls.Add(this.chkFF);
             this.flpDungeons.Controls.Add(this.chkET);
             this.flpDungeons.Controls.Add(this.chkWT);
-            this.flpDungeons.Location = new System.Drawing.Point(360, 3);
+            this.flpDungeons.Location = new System.Drawing.Point(360, 4);
+            this.flpDungeons.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.flpDungeons.Name = "flpDungeons";
-            this.flpDungeons.Size = new System.Drawing.Size(440, 102);
+            this.flpDungeons.Size = new System.Drawing.Size(440, 128);
             this.flpDungeons.TabIndex = 4;
             this.flpDungeons.Visible = false;
+            // 
+            // lblDungeons
+            // 
+            this.lblDungeons.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDungeons.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDungeons.Location = new System.Drawing.Point(3, 0);
+            this.lblDungeons.Name = "lblDungeons";
+            this.lblDungeons.Size = new System.Drawing.Size(437, 30);
+            this.lblDungeons.TabIndex = 7;
+            this.lblDungeons.Text = "Active Dungeons :";
             // 
             // chkDRC
             // 
             this.chkDRC.AutoSize = true;
-            this.chkDRC.Location = new System.Drawing.Point(3, 27);
+            this.chkDRC.Location = new System.Drawing.Point(3, 34);
+            this.chkDRC.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkDRC.Name = "chkDRC";
-            this.chkDRC.Size = new System.Drawing.Size(167, 21);
+            this.chkDRC.Size = new System.Drawing.Size(171, 24);
             this.chkDRC.TabIndex = 1;
             this.chkDRC.Text = "Dragon Roast Cavern";
             this.chkDRC.UseVisualStyleBackColor = true;
@@ -164,9 +191,10 @@
             // chkFW
             // 
             this.chkFW.AutoSize = true;
-            this.chkFW.Location = new System.Drawing.Point(176, 27);
+            this.chkFW.Location = new System.Drawing.Point(180, 34);
+            this.chkFW.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkFW.Name = "chkFW";
-            this.chkFW.Size = new System.Drawing.Size(142, 21);
+            this.chkFW.Size = new System.Drawing.Size(149, 24);
             this.chkFW.TabIndex = 2;
             this.chkFW.Text = "Forbidden Woods";
             this.chkFW.UseVisualStyleBackColor = true;
@@ -174,9 +202,10 @@
             // chkTOTG
             // 
             this.chkTOTG.AutoSize = true;
-            this.chkTOTG.Location = new System.Drawing.Point(3, 54);
+            this.chkTOTG.Location = new System.Drawing.Point(3, 66);
+            this.chkTOTG.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkTOTG.Name = "chkTOTG";
-            this.chkTOTG.Size = new System.Drawing.Size(147, 21);
+            this.chkTOTG.Size = new System.Drawing.Size(152, 24);
             this.chkTOTG.TabIndex = 3;
             this.chkTOTG.Text = "Tower of the Gods";
             this.chkTOTG.UseVisualStyleBackColor = true;
@@ -184,9 +213,10 @@
             // chkFF
             // 
             this.chkFF.AutoSize = true;
-            this.chkFF.Location = new System.Drawing.Point(156, 54);
+            this.chkFF.Location = new System.Drawing.Point(161, 66);
+            this.chkFF.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkFF.Name = "chkFF";
-            this.chkFF.Size = new System.Drawing.Size(145, 21);
+            this.chkFF.Size = new System.Drawing.Size(144, 24);
             this.chkFF.TabIndex = 4;
             this.chkFF.Text = "Forsaken Fortress";
             this.chkFF.UseVisualStyleBackColor = true;
@@ -194,9 +224,10 @@
             // chkET
             // 
             this.chkET.AutoSize = true;
-            this.chkET.Location = new System.Drawing.Point(307, 54);
+            this.chkET.Location = new System.Drawing.Point(311, 66);
+            this.chkET.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkET.Name = "chkET";
-            this.chkET.Size = new System.Drawing.Size(115, 21);
+            this.chkET.Size = new System.Drawing.Size(118, 24);
             this.chkET.TabIndex = 5;
             this.chkET.Text = "Earth Temple";
             this.chkET.UseVisualStyleBackColor = true;
@@ -204,44 +235,25 @@
             // chkWT
             // 
             this.chkWT.AutoSize = true;
-            this.chkWT.Location = new System.Drawing.Point(3, 81);
+            this.chkWT.Location = new System.Drawing.Point(3, 98);
+            this.chkWT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chkWT.Name = "chkWT";
-            this.chkWT.Size = new System.Drawing.Size(113, 21);
+            this.chkWT.Size = new System.Drawing.Size(119, 24);
             this.chkWT.TabIndex = 6;
             this.chkWT.Text = "Wind Temple";
             this.chkWT.UseVisualStyleBackColor = true;
             // 
-            // chkRaceMode
+            // FormSettings
             // 
-            this.chkRaceMode.AutoSize = true;
-            this.chkRaceMode.Location = new System.Drawing.Point(3, 74);
-            this.chkRaceMode.Name = "chkRaceMode";
-            this.chkRaceMode.Size = new System.Drawing.Size(102, 21);
-            this.chkRaceMode.TabIndex = 3;
-            this.chkRaceMode.Text = "Race Mode";
-            this.chkRaceMode.UseVisualStyleBackColor = true;
-            this.chkRaceMode.CheckedChanged += new System.EventHandler(this.chkRaceMode_CheckedChanged);
-            // 
-            // lblDungeons
-            // 
-            this.lblDungeons.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblDungeons.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDungeons.Location = new System.Drawing.Point(3, 0);
-            this.lblDungeons.Name = "lblDungeons";
-            this.lblDungeons.Size = new System.Drawing.Size(437, 24);
-            this.lblDungeons.TabIndex = 7;
-            this.lblDungeons.Text = "Active Dungeons :";
-            // 
-            // SettingsForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 562);
             this.Controls.Add(this.flpMenu);
             this.Controls.Add(this.flpMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "SettingsForm";
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormSettings";
             this.Text = "BingoGenerator";
             this.flpMenu.ResumeLayout(false);
             this.flpLogic.ResumeLayout(false);
--- LibBingo/Category.cs
+++ LibBingo/Category.cs
@@ -1,38 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibBingo
+﻿namespace LibBingo
 {
     public class Category
     {
         private string name;
         private bool isActive;
-        #nullable enable
         private Category? parent;
-        #nullable disable
 
         public string Name { get => name; }
         public bool IsActive {
             get => parent == null ? isActive : isActive && parent.IsActive;
             set => isActive = value;
         }
-
-        #nullable enable
         public Category? Parent{ get => parent; }
 
-        internal void SetParent(Category category)
+        internal void SetParent(Category? category = null)
         {
             parent = category;
         }
-
-        internal void SetParent()
-        {
-            parent = null;
-        }
-        #nullable disable
 
         public Category(string name)
         {
